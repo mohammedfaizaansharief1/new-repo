c5daad69815056855532e245d63d5e62563d13bc
mohammmedfaizaansharief <mohdfaizaansharief@gmail.com>
2023-12-08 01:57:37 +0530

    main

--- C_Language/a.c
+++ C_Language/a.c
@@ -1,4 +1,22 @@
 #include<stdio.h>
 void main(){
-    printf("Hello World");
-}
+    int i,x[10],min,max,sum=0;
+    float avg;
+    printf("Enter the 10 numbers:\n");
+    for(i=0;i<10;i++){
+        scanf("%d",&x[i]);
+    }
+    max = x[0];
+    min = x[0];
+    for(i=0;i<10;i++){
+        if(x[i]>max){
+            max = x[i];
+        }
+        if(x[i]<min){
+            min = x[i];
+        }
+        sum = sum+x[i];
+    } //end of for loop
+    avg = (float)sum/10.0;
+    printf("max = %d  min = %d  avg = %f",max,min,avg);
+} //end of main()
