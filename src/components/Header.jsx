3e43c1e5665d5343247e70763c6b0f888f864e18
mohammedfaizaansharief1 <mohdfaizaansharief@gmail.com>
2024-01-24 09:46:23 +0530

    added_youtbue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import Logo from "../assets/yt-logo.png";
 
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useUserContext } from "../context/AddUserContext";
-import {Link} from "react-router-dom"
-import { useFetch } from '../hooks/useFetch';
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 // import 'dotenv/config'
 
 const Header = () => {
-  const [searchVal,setSearchVal] = useState("")
-  let [resData,setResData] = useState("")
+  const [searchVal, setSearchVal] = useState("");
+  let [resData, setResData] = useState("");
   let { user } = useUserContext();
-  let [suggValues,setSuggValues] = useState(false)
+  let [suggValues, setSuggValues] = useState(false);
 
   // const apiKey = process.env.Youtube_Api_Key;
   // const apiUrl = process.env.REACT_APP_API_URL;
@@ -29,43 +29,47 @@ const Header = () => {
         // An error happened.
       });
   }
-  async function ResVideoList(){
+  async function ResVideoList() {
     let res = await useFetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=${(import.meta.env).VITE_REACT_API_KEY}`
-    )
-    setResData(res.items)
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=${
+        import.meta.env.VITE_REACT_API_KEY
+      }`
+    );
+    setResData(res.items);
   }
-      // async function fetchData(){
-    //     let data = await fetch(
-    //       "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=30&regionCode=IN&key=AIzaSyCS8mGrSJW8BEopuoQDXMxAn5vOs6ZEjYg"
-    //     )
-    //     let res = await data.json()
-    //     setResData(res.items)
-    // }
-    useEffect(()=>{
-      // console.log(resData)
-      let a =setTimeout(()=>{
-        if(searchVal.length >=1){
-          // fetchData()
-          ResVideoList()
-        }
-      },300)
-      return()=>{
-        clearTimeout(a)
+  // async function fetchData(){
+  //     let data = await fetch(
+  //       "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=30&regionCode=IN&key=AIzaSyCS8mGrSJW8BEopuoQDXMxAn5vOs6ZEjYg"
+  //     )
+  //     let res = await data.json()
+  //     setResData(res.items)
+  // }
+  useEffect(() => {
+    // console.log(resData)
+    let a = setTimeout(() => {
+      if (searchVal.length >= 1) {
+        // fetchData()
+        ResVideoList();
       }
-    },[searchVal])
-    console.log(resData)
+    }, 300);
+    return () => {
+      clearTimeout(a);
+    };
+  }, [searchVal]);
+  console.log(resData);
 
-    function searchHandler(e){
-      setSearchVal(e.target.value)
-      setSuggValues(true)
-    }
+  function searchHandler(e) {
+    setSearchVal(e.target.value);
+    setSuggValues(true);
+  }
 
   return (
     <div className="p-5 flex justify-between items-center">
-      <div>
-        <img src={Logo} alt="" width="100" height="100" />
-      </div>
+      <Link to="/home">
+        <div>
+          <img src={Logo} alt="" width="100" height="100" />
+        </div>
+      </Link>
 
       <div>
         <input
@@ -74,11 +78,11 @@ const Header = () => {
           name="Search"
           id=""
           value={searchHandler}
-          onFocus={()=>{
-            setSuggValues(true)
+          onFocus={() => {
+            setSuggValues(true);
           }}
-          onBlur={()=>{
-            setSuggValues(false)
+          onBlur={() => {
+            setSuggValues(false);
           }}
         />
       </div>
@@ -93,15 +97,14 @@ const Header = () => {
             <div>
               {suggValues && (
                 <div>
-                  {resData.localeCompare((e)=>{
+                  {resData.localeCompare((e) => {
                     <Link key={e.id} to={`watch?v=${e.id}`}>
                       <div>{e.snippet.title}</div>
-                    </Link>
+                    </Link>;
                   })}
                 </div>
               )}
             </div>
-            
           </>
         )}
       </div>
